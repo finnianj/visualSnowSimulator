fcfce98bcb2bcb2afaa263f87238b1d2e8af2177
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2024-01-26 11:23:47 +0100

    Update snow softness and size parameters

--- src/shaders/snow/fragment.glsl
+++ src/shaders/snow/fragment.glsl
@@ -1,19 +1,24 @@
 uniform float uTime; // Time uniform
 uniform float uBrightness; // Brightness uniform
 uniform float uSoftness; // Softness uniform
+uniform float uSize; // Size uniform
 varying vec3 vColor;
 
 void main()
 {
     // Light point
     float distanceFromCenter = distance(gl_PointCoord, vec2(0.5));
+    // Inverts values so center is 1.0, edge is 0.5
     distanceFromCenter = 1.0 - distanceFromCenter;
-    distanceFromCenter = pow(distanceFromCenter, 50.0);
+    // Makes edges equivalent to 0 and creates a gradual fade by raising values
+    distanceFromCenter = pow(distanceFromCenter, 10.0);
 
-    // Calculate the alpha value based on the softness
-    float alpha = 1.0; // Default: Hard circle
-    alpha = smoothstep(0.5 - uSoftness, 0.5 + uSoftness, distanceFromCenter);
+    // At this point you have a gradual fade which you can use as the alpha value
+    // vec3 color = mix(vec3(0.0), vColor, distanceFromCenter);
 
+    // Or calculate the alpha value based on the softness using smoothstep
+    float alpha = distanceFromCenter;
+    alpha = smoothstep(0.01, 0.01 + uSoftness, distanceFromCenter);
 
     // Final color
     vec3 color = mix(vec3(0.0), vColor, alpha);
