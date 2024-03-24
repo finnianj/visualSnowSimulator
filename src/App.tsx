b141551ba1fa6d29cd753a3e88f63878b16996ba
finn <charltonjones.fj@gmailcom>
2024-03-24 11:53:11 +0100

    Add dynamic environment map and loading indicator

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, Suspense, startTransition, useMemo, useCallback } from 'react';
 import { Canvas, useFrame } from "@react-three/fiber";
 
 import { Environment, useEnvironment, OrbitControls } from '@react-three/drei'
@@ -11,24 +11,35 @@ const mapsArray = [
 ]
 
 export default function App() {
-    const envMap = useEnvironment({ files: `./environmentMaps/hdri/${mapsArray[0]}.exr` })
+    const [mapIndex, setMapIndex] = useState(0)
+    const envMap = useMemo(() => useEnvironment({ files: `./environmentMaps/hdri/${mapsArray[mapIndex]}.exr` }), [mapIndex]);
+    const [loadingMap, setLoadingMap] = useState(false)
 
-    // const changeMap = () => {
-    //     const index = mapsArray.indexOf(map)
-    //     const newIndex = (index + 1) % mapsArray.length
-    //     setMap(mapsArray[newIndex])
-    // }
+    const changeMap = useCallback(() => {
+        setLoadingMap(true)
+            // setMapIndex((prevIndex) => (prevIndex + 1) % mapsArray.length);
+        startTransition(() => {
+            setTimeout(() => {
+                setMapIndex((prevIndex) => (prevIndex + 1) % mapsArray.length);
+                setLoadingMap(false)
+            }, 3000);
+        });
+    }, [mapsArray.length]);
 
 
 
     return (
        <>
+            <button onClick={changeMap} className='bg-teal-400 hover:bg-teal-500 transition-all text-white rounded-lg absolute z-50 p-4 m-4 shadow-lg'>Change Map</button>
+            {loadingMap && <div className='loading-indicator'>Loading...</div>}
             <Canvas>
                 <OrbitControls />
+                <Suspense fallback={<div className='text-4xl z-50 absolute'>Loading environment...</div>}>
                 <Environment
                     background
                     map={envMap}
                 />
+                </Suspense>
             </Canvas>
        </>
     );
