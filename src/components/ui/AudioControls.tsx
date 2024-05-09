1565d875e836b81976829f0fe2fb1762eff209d2
finn <charltonjones.fj@gmailcom>
2024-05-09 13:51:23 +0200

    chore: Update AudioControls component to use checkboxes for controlling audio playback

--- src/components/ui/AudioControls.tsx
+++ src/components/ui/AudioControls.tsx
@@ -9,13 +9,30 @@ export const AudioControls = () => {
     } = useAudio();
 
     return (
-        <div>
-            <button onClick={() => setIsAmbientPlaying(!isAmbientPlaying)}>
-                {isAmbientPlaying ? 'Pause Ambient' : 'Play Ambient'}
-            </button>
-            <button onClick={() => setIsEffectsAudioPlaying(!isEffectsAudioPlaying)}>
-                {isEffectsAudioPlaying ? 'Pause Effects' : 'Play Effects'}
-            </button>
+        <div className='flex flex-col text-xs space-y-2'>
+
+            <div className='flex items-center justify-between'>
+                <label htmlFor='ambientAudioCheckbox' className='cursor-pointer'>Ambient Audio</label>
+                <input 
+                    type='checkbox'
+                    id='ambientAudioCheckbox'
+                    onChange={() => setIsAmbientPlaying(!isAmbientPlaying)}
+                    checked={isAmbientPlaying}
+                    className='accent-teal-600 cursor-pointer'
+                />
+            </div>
+
+            <div className='flex items-center justify-between'>
+                <label htmlFor='effectsAudioCheckbox' className='cursor-pointer'>Effects Audio</label>
+                <input 
+                    type='checkbox'
+                    id='effectsAudioCheckbox'
+                    onChange={() => setIsEffectsAudioPlaying(!isEffectsAudioPlaying)}
+                    checked={isEffectsAudioPlaying}
+                    className='accent-teal-600 cursor-pointer'
+                />
+            </div>
+            
         </div>
         
     )
