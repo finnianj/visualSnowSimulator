66d07c6f45d5fa2a45ce25a57179f87360bac183
finn <charltonjones.fj@gmailcom>
2024-05-09 15:19:56 +0200

    chore: Update Loading component to display "Loading textures..." instead of "Loading environment..."

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -4,7 +4,7 @@ export default function Loading() {
     return (
         <div className='loading-indicator hover:cursor-progress'>
             <AiOutlineLoading3Quarters className='loading-icon animate-spin w-8 h-8' />
-            <p>Loading environment...</p>
+            <p>Loading textures...</p>
         </div>
     )
 }
