0f157d48d1c48ab9883b8ff58a2bb0eebc3f697c
finn <charltonjones.fj@gmailcom>
2024-06-22 12:20:36 +0200

    Re enable mobile support

--- src/index.tsx
+++ src/index.tsx
@@ -3,24 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './style.css'
 
 import { MobileNotSupported } from './components/ui/MobileNotSupported';
-const App = React.lazy(() => import('./App'));
+// const App = React.lazy(() => import('./App'));
+import App from './App';
 
 const rootElement = document.getElementById('root')!;
 const root = ReactDOM.createRoot(rootElement);
-const isMobile = window.innerWidth < 768;
+// const isMobile = window.innerWidth < 768;
 
-if (isMobile) {
-  root.render(
-    <React.StrictMode>
-      <MobileNotSupported />
-    </React.StrictMode>
-  );
+// if (isMobile) {
+//   root.render(
+//     <React.StrictMode>
+//       <MobileNotSupported />
+//     </React.StrictMode>
+//   );
 
-} else {
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
+// } else {
+//   root.render(
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>
+//   );
+// }
 
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
